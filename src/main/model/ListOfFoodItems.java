d224a5a8aed44fbac89642d45f78636fd53c6074
Ryan Vu <ryanvu1131@gmail.com>
2024-10-04 23:16:47 -0700

    Implemented LIstOfFoodItems Class

--- src/main/model/ListOfFoodItems.java
+++ src/main/model/ListOfFoodItems.java
@@ -1,46 +1,50 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfFoodItems {
 
+    private List<FoodItems> listFoodItems;
+
     // EFFECTS : constructs empty list of food items
-    void ListOfFoodItems() {
-        // TODO
+    public ListOfFoodItems() {
+        listFoodItems = new ArrayList<FoodItems>();
     }
 
     // MODIFIES: this
     // EFFECTS: adds food to list of food items
     public void addFood(FoodItems food) {
-        // TODO
+        listFoodItems.add(food);
     }
 
     // MODIFIES: this
     // EFFECTS: removes food from list of food items
     public void removeFood(FoodItems food) {
-        // TODO
+        listFoodItems.remove(food);
     }
 
     // EFFECTS: returns list of food items
     public List<FoodItems> getListOfFoodItems() {
-        return null;
-        // TODO
+        return listFoodItems;
     }
 
     // EFFECTS: returns number of food items consumed today
     public int sizeOfList() {
-        return 0;
-        // TODO
+        return listFoodItems.size();
     }
 
     // EFFECTS: returns total number of calories consumed
     public int totalNumOfCalories() {
-        return 0;
-        // TODO
+        int calories = 0;
+        for (FoodItems fi : listFoodItems) {
+            calories += fi.getCalories();
+        }
+        return calories;
     }
 
     // EFFECTS: clears the list
     public void clearList() {
-        // TODO
+        listFoodItems.clear();
     }
 }
