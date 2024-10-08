ed002bbd5b5a875fe68364db0bf090c8f2bb4215
Ryan Vu <ryanvu1131@gmail.com>
2024-10-08 16:35:44 -0700

    Created Specifications for Calories Class

---
+++ src/main/model/Calories.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Calories {
+
+    // EFFECTS: Constructs object with calorie goal and today's date
+    public Calories() {
+        // TODO
+    }
+
+    // EFFECTS: returns calories goals
+    public int getCalorieGoal() {
+        return 0;
+        // TODO
+    }
+
+    // MODIFIES; this
+    // EFFECTS: sets new calorie goal
+    public void setNewGoal() {
+        // TODO
+    }
+
+    // EFFECTS: determines difference between amount consumed and amount burned
+    public int difference(ListOfFoodItems lofi, ListExercise loe) {
+        // TODO
+        return 0;
+    }
+
+    // EFFECTS: returns how much calories are required to reach your goal
+    public int neededAmound() {
+        return 0;
+        // TODO
+    }
+
+    // EFFECTS: returns how many calories you went over 
+    public int overAmount() {
+        return 0;
+        // TODO
+    }
+
+    public LocalDate getDate() {
+        return null;
+        // TODO
+    }
+
+    public void setDate() {
+        // TODO
+    }
+}
