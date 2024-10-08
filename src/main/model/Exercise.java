8dc3fa138d46d56a5a655970486d7b010344ebb3
Ryan Vu <ryanvu1131@gmail.com>
2024-10-07 21:20:09 -0700

    Created implementation for Exercise Class

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -1,34 +1,36 @@
 package model;
 
 public class Exercise {
+
+    private String exercise;
+    private int caloriesburnt;
     
     // EFFECTS: constucts exercise item with name of exercise and number of calories it burns
     public Exercise(String exercise, int calories) {
-        // TODO
+        this.exercise = exercise;
+        caloriesburnt = calories;
     }
 
     // EFFECTS: returns name of exercise
     public String getExercise() {
-        return null;
-        // TODO
+        return exercise;
     }
 
     // EFFECTS: returns number of calories burned
     public int getCaloriesBurned() {
-        return 0;
-        // TODO
+        return caloriesburnt;
     }
 
     // MODIFIES: this
     // EFFECTS: edits number of calories burned
     public void editCalories(int calories) {
-        // TODO
+        caloriesburnt = calories;
     }
 
     // MODIFIES: this
     // EFFECTS: edits exercise name
     public void editExercise(String exercise) {
-        // TODO
+        this.exercise = exercise;
     }
 
 
