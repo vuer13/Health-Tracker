6ee7cc172bf905babb1c592442c8d0a66aa829a3
Ryan Vu <ryanvu1131@gmail.com>
2024-10-08 11:32:00 -0700

    Created Specifications for ListExercise Class

---
+++ src/main/model/ListExercise.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
+public class ListExercise {
+    
+    // EFFECTS: constructs list of exercise with date associate with it
+    public ListExercise() {
+        // TODO
+    }
+
+    // EFFECTS: returns list of Exercise
+    public void getListExercise() {
+        // TODO
+    }
+
+    // EFFECTS: returns total number of calories burnt
+    public int getTotalCaloriesBurnt() {
+        return 0;
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds exercise to the list
+    public void addExercise(Exercise e) {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes exercise to the list
+    public void removeExercise(Exercise e) {
+        // TODO
+    }
+
+    // EFFECTS: returns date of list
+    public LocalDate getDate() {
+        return null;
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets date of list to today's list
+    public void setDate(){
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: clears list
+    public void clearList() {
+        // TODO
+    }
+
+    // EFFECTS: returns size of list
+    public int sizeList() {
+        return 0;
+        // TODO
+    }
+}
