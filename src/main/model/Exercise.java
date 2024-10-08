25626969972e2f52b08446d1b86cafa8df537a6d
Ryan Vu <ryanvu1131@gmail.com>
2024-10-07 21:10:39 -0700

    Created specifications for Exercise Class

---
+++ src/main/model/Exercise.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Exercise {
+    
+    // EFFECTS: constucts exercise item with name of exercise and number of calories it burns
+    public Exercise(String exercise, int calories) {
+        // TODO
+    }
+
+    // EFFECTS: returns name of exercise
+    public String getExercise() {
+        return null;
+        // TODO
+    }
+
+    // EFFECTS: returns number of calories burned
+    public int getCaloriesBurned() {
+        return 0;
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: edits number of calories burned
+    public void editCalories() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: edits exercise name
+    public void editExercise() {
+        // TODO
+    }
+
+
+}
