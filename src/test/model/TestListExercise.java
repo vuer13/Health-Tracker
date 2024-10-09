365a00575324847417282aa6e9269a82784fc293
Ryan Vu <ryanvu1131@gmail.com>
2024-10-09 10:38:38 -0700

    Removed unnecessary import statements

--- src/test/model/TestListExercise.java
+++ src/test/model/TestListExercise.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.time.LocalDate;
 
 public class TestListExercise {
