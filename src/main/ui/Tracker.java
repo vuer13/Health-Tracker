bbd3424a91f88ba7c203f2db9a255f359b17d570
Ryan Vu <ryanvu1131@gmail.com>
2024-10-08 22:40:16 -0700

    Created Specifications for Tracker class

---
+++ src/main/ui/Tracker.java
@@ -0,0 +1,41 @@
+package ui;
+
+public class Tracker {
+    
+
+    // EFFECTS: Initiates UI console base
+    public Tracker() {
+        // TODO
+    }
+
+    // EFFECTS: displays and processes inputs for main menu
+    public void handleMenu() {
+        // TODO
+    }
+
+    // EFFECTS: displays main menu
+    public void displayMenu() {
+        // TODO
+    }
+
+    // EFFECTS: processes menu inputs
+    public void processMenu() {
+        // TODO
+    }
+
+    // EFFECTS: creates new food item
+    public void createFood() {
+        // TODO
+    }
+
+    // EFFECTS: creates new exercise
+    public void createExercise() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: quits application
+    public void quitApp() {
+        // TODO
+    }
+}
