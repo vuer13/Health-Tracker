e6bb3f6f3a0c67393f7de80c894251be3800944c
Ryan Vu <ryanvu1131@gmail.com>
2024-10-24 11:54:59 -0700

    Added class document

--- src/main/persistance/Writeable.java
+++ src/main/persistance/Writeable.java
@@ -2,6 +2,7 @@ package persistance;
 
 import org.json.JSONObject;
 
+// Interface with method that returns as JSON object
 public interface Writeable {
     
     // EFFECTS: returns this as JSON object
