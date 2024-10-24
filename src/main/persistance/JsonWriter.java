71ed9644441b3b3b9f9e2ee1bac9c29f108318b8
Ryan Vu <ryanvu1131@gmail.com>
2024-10-24 00:42:39 -0700

    Finished Implementation for JsonWriter, still requires additional implementation for lofi and loe before tests can be performed

--- src/main/persistance/JsonWriter.java
+++ src/main/persistance/JsonWriter.java
@@ -3,7 +3,8 @@
 
 package persistance;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import org.json.JSONObject;
 
 import model.ListExercise;
 import model.ListOfFoodItems;
@@ -11,33 +12,41 @@ import model.ListOfFoodItems;
 // Represents writer that writes JSON representation of list of food items/exercises to file
 public class JsonWriter {
     
+    private static final int TAB = 4;
+
+    private String destination;
+    private PrintWriter writer;
+
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // TODO
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer
     // throws FileNotFoundException if destination file cannot be opened for writing
     public void open() throws FileNotFoundException {
-        // TODO
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of list of exercises and food items to file
     public void write(ListExercise loe, ListOfFoodItems lofi) {
-        // TODO
+        JSONObject jsone = loe.toJson();
+        JSONObject jsonf = lofi.toJson();
+        saveToFile(jsone.toString(TAB));
+        saveToFile(jsonf.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // TODO
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     public void saveToFile(String json) {
-        // TODO
+        writer.print(json);
     }
 }
