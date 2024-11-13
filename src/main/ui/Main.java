f53ef6b9a37f862fe91ee1cbc494a53d41d15a67
Ryan Vu <ryanvu1131@gmail.com>
2024-11-13 14:41:04 -0800

    Created specification for Main class

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,7 +3,7 @@ package ui;
 // Opens panel up
 public class Main {
     
-    // EFFECTS: creates new TrackerUI instance, using Java Swing Library
+    // EFFECTS: creates new TrackerUI instance, using Java Swing
     public static void main(String[] args) throws Exception {
         // No Longer Required:
         // new Tracker(); 
