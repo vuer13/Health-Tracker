00c918b594dd494884af28d3e51b793d43a84c80
Ryan Vu <ryanvu1131@gmail.com>
2024-11-13 14:37:00 -0800

    Specified TrackerUI Class

---
+++ src/main/ui/TrackerUI.java
@@ -0,0 +1,48 @@
+package ui;
+
+import javax.swing.JFrame;
+
+// GUI for program
+public class TrackerUI extends JFrame {
+
+    // MODIFIES: this
+    // EFFECTS: Sets Up GUI
+    public TrackerUI() {
+        // TODO
+    }
+
+    // EFFECTS: displays starting menu with option to load previous mode
+    private void startMenu() {
+        // TODO
+    }
+
+    // MODIFIES: calories
+    // EFFECTS: creates menu allowing user to make calorie goal
+    private void goalMenu() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates new list of exercises
+    private void loadNewExercises() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads list of exercises
+    private void loadExercises() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates new list of food items from previously saved items
+    private void loadNewFoods() {
+        // TODO
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads list of food items from previously saved items
+    private void loadFoods() {
+        // TODO
+    }
+}
