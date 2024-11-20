a9419abd77f1c1e2dada41121b6938671dab861e
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 17:00:09 -0800

    Added new specification to addExButton

--- src/main/ui/TrackerUI.java
+++ src/main/ui/TrackerUI.java
@@ -452,6 +452,13 @@ public class TrackerUI extends JFrame {
                 clearFoodPanel();
             }
         });
+
+        addExButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO
+            }
+        });
     }
 
     // EFFECTS: pops window open, giving user option to load previously saved data
