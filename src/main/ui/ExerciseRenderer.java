5a053689442079b1eab05a01bca64cde83debdb0
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 21:53:03 -0800

    Implemented ExerciseRendered class

--- src/main/ui/ExerciseRenderer.java
+++ src/main/ui/ExerciseRenderer.java
@@ -3,6 +3,8 @@ package ui;
 import java.awt.*;
 import javax.swing.*;
 
+import model.Exercise;
+
 // Custom cell renderer for rendering Exercise objects in JList
 public class ExerciseRenderer extends DefaultListCellRenderer {
     
@@ -11,8 +13,13 @@ public class ExerciseRenderer extends DefaultListCellRenderer {
      public Component getListCellRendererComponent(
              JList<?> list, Object value, int index,
              boolean isSelected, boolean cellHasFocus) {
-        // TODO
-        return null;
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        Exercise ex = (Exercise) value;
+        String name = ex.getExercise();
+        int cal = ex.getCaloriesBurned();
+        String exerciseString = "Name: " + name + ", Calories Burnt: " + cal;
+        setText(exerciseString);
+        return this;
     }
 
 }
