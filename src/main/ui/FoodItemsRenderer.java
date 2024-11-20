b299e89635266b164f519d131e864e94c3bed467
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 21:10:15 -0800

    Created new class for renderering of fooditems list (experimenting with is)

---
+++ src/main/ui/FoodItemsRenderer.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+// Custom cell renderer for rendering FoodItems objects in JList
+public class FoodItemsRenderer extends DefaultListCellRenderer {
+
+    
+
+}
