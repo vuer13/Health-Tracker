b6a62781c8c2f3f4a6a2632e2057fc4358f11902
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 21:55:04 -0800

    Edited specifactions

--- src/main/ui/ExerciseRenderer.java
+++ src/main/ui/ExerciseRenderer.java
@@ -7,12 +7,13 @@ import model.Exercise;
 
 // Custom cell renderer for rendering Exercise objects in JList
 public class ExerciseRenderer extends DefaultListCellRenderer {
-    
-     // EFFECTS: Configures rendering for FoodItems objects in JList
-     @Override
-     public Component getListCellRendererComponent(
-             JList<?> list, Object value, int index,
-             boolean isSelected, boolean cellHasFocus) {
+
+    // MODIFIES: list
+    // EFFECTS: Configures rendering for FoodItems objects in JList
+    @Override
+    public Component getListCellRendererComponent(
+            JList<?> list, Object value, int index,
+            boolean isSelected, boolean cellHasFocus) {
         super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         Exercise ex = (Exercise) value;
         String name = ex.getExercise();
--- src/main/ui/FoodItemsRenderer.java
+++ src/main/ui/FoodItemsRenderer.java
@@ -10,6 +10,7 @@ import java.awt.*;
 // Custom cell renderer for rendering FoodItems objects in JList
 public class FoodItemsRenderer extends DefaultListCellRenderer {
 
+    // MODIFIES: list
     // EFFECTS: Configures rendering for FoodItems objects in JList
     @Override
     public Component getListCellRendererComponent(
