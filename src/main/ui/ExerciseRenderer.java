200ba992889d4dcb049cd651e4c62a2248280e0a
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 21:51:02 -0800

    Comepleted specifcation for ExerciseRenderer

--- src/main/ui/ExerciseRenderer.java
+++ src/main/ui/ExerciseRenderer.java
@@ -1,8 +1,18 @@
 package ui;
 
+import java.awt.*;
 import javax.swing.*;
 
 // Custom cell renderer for rendering Exercise objects in JList
 public class ExerciseRenderer extends DefaultListCellRenderer {
     
+     // EFFECTS: Configures rendering for FoodItems objects in JList
+     @Override
+     public Component getListCellRendererComponent(
+             JList<?> list, Object value, int index,
+             boolean isSelected, boolean cellHasFocus) {
+        // TODO
+        return null;
+    }
+
 }
