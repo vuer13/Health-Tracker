80a621eedeeade05d1b4c21b1bd0e5bb0fc1821c
Ryan Vu <ryanvu1131@gmail.com>
2024-11-19 21:15:26 -0800

    Created implementation for the method

--- src/main/ui/FoodItemsRenderer.java
+++ src/main/ui/FoodItemsRenderer.java
@@ -6,6 +6,13 @@ import java.awt.*;
 // Custom cell renderer for rendering FoodItems objects in JList
 public class FoodItemsRenderer extends DefaultListCellRenderer {
 
-    
+    // EFFECTS: Configures rendering for FoodItems objects in JList
+    @Override
+    public Component getListCellRendererComponent(
+            JList list, Object value, int index,
+            boolean isSelected, boolean cellHasFocus) {
+        // TODO
+        return null;
+    }
 
 }
